3680d17be9732a0191edb9f5a3fe8be2f450edad
Chris Steinle <steinle.4400@gmail.com>
2018-04-10 23:11:58 -0700

    grabs data from api, displays unstyled view, stores in var while server running, can push user onto going or not going, displays json of going and not going when at their respective paths

--- app.js
+++ app.js
@@ -1,11 +1,72 @@
-var express = require('express')
-var path = require('path')
-var bodyParser = require('body-parser')
-var mustacheExpress = require('mustache-express')
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const mustacheExpress = require('mustache-express')
+const request = require('request')
+const apiurl = 'https://randomuser.me/api/'
+const userList = {users: {
+	going: [],
+	notgoing: []
+}}
+const app = express()
 
-var app = express()
-
+app.engine('mustache', mustacheExpress())
+app.set('view engine', 'mustache')
+app.set('views', __dirname + '/views')
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
-module.exports = app
+
+
+
+
+app.use((req,res,next)=>{
+	req.Demo = 'middleware test'
+	next()
+})
+
+app.get('/', (req,res,next) =>{
+
+	request.get({url: apiurl}, (err, resp, body) => {
+		let userdata = JSON.parse(body).results[0]
+		let uname = userdata.name.first + ' ' + userdata.name.last
+		req.User = {
+			name: uname,
+			phone: userdata.phone,
+			email: userdata.email,
+			picture: userdata.picture.large
+		}
+		next()
+	})
+})
+app.post('/mark-invitee', (req,res,next)=>{
+	// console.log('logging  body: ', req.body)
+	if(req.body.going === 'true'){
+		userList.users.going.push(req.body)
+	}
+	if(req.body.going === 'false'){
+		userList.users.notgoing.push(req.body)
+	}
+	res.redirect('/')
+})
+
+app.use('/going', (req,res,next) =>{
+	res.json(userList.users.going)
+})
+app.use('/notgoing', (req,res,next) =>{
+	res.json(userList.users.notgoing)
+})
+
+app.use('/', (req,res,next) =>{
+	res.render('main', req.User)
+})
+
+
+
+
+
+app.listen(8000, ()=>{
+	console.log("listening on port 8000")
+})
+// module.exports = app
